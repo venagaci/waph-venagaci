3dcbae659e59aa68ac7eac34cbb8ec7fa11f384f
Charan Sai Venaganti <152041684+venagaci@users.noreply.github.com>
2024-01-31 18:08:28 -0500

    Add files via upload

---
+++ labs/lab2/email.js
@@ -0,0 +1,14 @@
+var shown=false;
+function showhideEmail()
+{
+  if (shown){
+  	document.getElementById('email').InnerHTML = "show my email";
+  	shown = false;
+  }else{
+  var myemail= "<a href='mailto:venagaci" + "@" +
+               "mail.uc.edu'>venagaci" + "@" + "mail.uc.edu</a>";
+    document.getElementById('email').innerHTML = myemail;
+    shown = true;           
+  }
+
+}
