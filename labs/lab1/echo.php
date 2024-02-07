a4801fd4cf4d86e0305eb07bd108b197072c2e01
Charan Sai Venaganti <152041684+venagaci@users.noreply.github.com>
2024-02-07 18:21:25 -0500

    Delete labs/lab1/echo.php

--- labs/lab1/echo.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    if(empty($_REQUEST["data"])){
-     exit("please enter the input field 'data'");
-     }
-     $input=htmlentities($_REQUEST["data"]);
-     echo ("The input from the request is <strong>" .$input. "</strong>.<br>");
-
-?>
