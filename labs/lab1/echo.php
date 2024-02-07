933f91324c2ea6e66469e4f134b4b520be86836e
Charan Sai Venaganti <152041684+venagaci@users.noreply.github.com>
2024-02-07 18:23:28 -0500

    Revised echo.php

--- labs/lab1/echo.php
+++ labs/lab1/echo.php
@@ -1,3 +1,9 @@
 <?php 
-    $inputData=$_REQUEST["data"];
+    
+     if(empty($_REQUEST["data"])){
+     exit("please enter the input field 'data'");
+     } 
+     $input=htmlentities($_REQUEST["data"]); 
+     echo ("The input from the request is <strong>" .$input. "</strong>.<br>");
+    
 ?>
